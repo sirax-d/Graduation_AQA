24b142329bc3157add4e31b29b4f5c314806d264
Sergey Krichanov <133060553+sirax-d@users.noreply.github.com>
2024-03-05 23:19:56 +0100

    Update base_page.py

--- superjob_project/pages/mobile/base_page.py
+++ superjob_project/pages/mobile/base_page.py
@@ -1,3 +1,4 @@
+import allure
 from appium.webdriver.common.appiumby import AppiumBy
 from selene import have, be
 from selene.support.shared.jquery_style import s, ss
@@ -6,33 +7,39 @@ from selene.support.shared.jquery_style import s, ss
 class BasePage:
 
     def info(self):
-        s((AppiumBy.ID, 'ru.superjob.client.android:id/closeButton')).click()
-        s((AppiumBy.ID, 'ru.superjob.client.android:id/actionMore')).click()
-        s((AppiumBy.XPATH, '//android.widget.TextView[@text="О компании"]')).click()
+        with allure.step('Переходим в информацию о компании SJ без авторизации'):
+            s((AppiumBy.ID, 'ru.superjob.client.android:id/closeButton')).click()
+            s((AppiumBy.ID, 'ru.superjob.client.android:id/actionMore')).click()
+            s((AppiumBy.XPATH, '//android.widget.TextView[@text="О компании"]')).click()
 
     def company_info_check(self):
-        s((AppiumBy.XPATH,
-           '//android.widget.TextView[@resource-id="ru.superjob.client.android:id/companyHeaderDescription"]')) \
-            .should(have.text('Клиент SuperJob с 2000 года'))
+        with allure.step('Проверяем наличие информации о компании'):
+            s((AppiumBy.XPATH,
+               '//android.widget.TextView[@resource-id="ru.superjob.client.android:id/companyHeaderDescription"]')) \
+                .should(have.text('Клиент SuperJob с 2000 года'))
 
     def response(self):
-        s((AppiumBy.ID, 'ru.superjob.client.android:id/closeButton')).click()
-        s((AppiumBy.ID, 'ru.superjob.client.android:id/actionResponses')).click()
+        with allure.step('Переходим в  меню откликов неавторизованным пользователем'):
+            s((AppiumBy.ID, 'ru.superjob.client.android:id/closeButton')).click()
+            s((AppiumBy.ID, 'ru.superjob.client.android:id/actionResponses')).click()
 
     def response_check(self):
-        s((AppiumBy.CLASS_NAME, 'android.widget.TextView')).should(have.text('Здесь будут ваши отклики'))
-        s((AppiumBy.XPATH,
-           '//android.widget.ScrollView/android.view.View/android.view.View/android.widget.Button')).matching(
-            be.visible)
+        with allure.step('Проверяем наличие информации в меню откликов'):
+            s((AppiumBy.CLASS_NAME, 'android.widget.TextView')).should(have.text('Здесь будут ваши отклики'))
+            s((AppiumBy.XPATH,
+               '//android.widget.ScrollView/android.view.View/android.view.View/android.widget.Button')).matching(
+                be.visible)
 
     def find_vacancy_without_login(self):
-        s((AppiumBy.ID, 'ru.superjob.client.android:id/closeButton')).click()
-        s((AppiumBy.XPATH, '//android.widget.TextView[@text="Поиск работы"]')).click()
-        s((AppiumBy.CLASS_NAME, 'android.widget.EditText')).type('Python')
-        ss((AppiumBy.CLASS_NAME, 'android.widget.TextView'))[1].click()
+        with allure.step('Ищем вакансию без авторизации'):
+            s((AppiumBy.ID, 'ru.superjob.client.android:id/closeButton')).click()
+            s((AppiumBy.XPATH, '//android.widget.TextView[@text="Поиск работы"]')).click()
+            s((AppiumBy.CLASS_NAME, 'android.widget.EditText')).type('Python')
+            ss((AppiumBy.CLASS_NAME, 'android.widget.TextView'))[1].click()
 
     def check_results_vacancy(self):
-        s((AppiumBy.CLASS_NAME, 'android.widget.TextView')).should(have.text('ВАКАНСИЙ'))
+        with allure.step('Проверяем результаты поиска'):
+            s((AppiumBy.CLASS_NAME, 'android.widget.TextView')).should(have.text('ВАКАНСИЙ'))
 
 
 base_page = BasePage()
