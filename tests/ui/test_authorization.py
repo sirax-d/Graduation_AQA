f24c3d9a091d854628aa1c2ee68a7a196d4402ee
Sergey Krichanov <133060553+sirax-d@users.noreply.github.com>
2024-03-05 23:21:44 +0100

    Update test_authorization.py

--- tests/ui/test_authorization.py
+++ tests/ui/test_authorization.py
@@ -12,10 +12,8 @@ from superjob_project.pages.ui.base_page import base
 @allure.severity('normal')
 @allure.label('layer', 'ui')
 def test_create_account():
-    with allure.step('Preconditions: open SJ site'):
-        base.open()
-    with allure.step('Create account and check successful registration depending of "if"'):
-        base.registration()
+    base.open()
+    base.registration()
 
 
 @allure.epic('Authorized')
@@ -26,13 +24,10 @@ def test_create_account():
 @allure.label('layer', 'ui')
 @pytest.mark.xfail(reason="Нестабильный тест")
 def test_login_and_response():
-    with allure.step('Preconditions: open SJ site'):
-        base.open()
-    with allure.step('Login in SJ site and create response to the vacancy'):
-        account.login()
-        account.create_response()
-    with allure.step('Check response'):
-        account.check_response()
+    base.open()
+    account.login()
+    account.create_response()
+    account.check_response()
 
 
 @allure.epic('Authorized')
@@ -43,13 +38,10 @@ def test_login_and_response():
 @allure.label('layer', 'ui')
 @pytest.mark.xfail(reason="Нестабильный тест, динамические окна")
 def test_create_resume():
-    with allure.step('Preconditions: open SJ site'):
-        base.open()
-    with allure.step('Login in SJ site and create resume'):
-        account.login()
-        account.create_resume()
-    with allure.step('Check create resume'):
-        account.check_create_resume()
+    base.open()
+    account.login()
+    account.create_resume()
+    account.check_create_resume()
 
 
 @allure.epic('Authorized')
@@ -59,14 +51,10 @@ def test_create_resume():
 @allure.severity('normal')
 @allure.label('layer', 'ui')
 def test_resume_visibility_off():
-    with allure.step('Preconditions: open SJ site'):
-        base.open()
-    with allure.step('Login in SJ site'):
-        account.login()
-    with allure.step('Create other resume, and close visibility.")'):
-        account.create_other_resume()
-    with allure.step('Check visibility of the resume'):
-        account.check_visibility()
+    base.open()
+    account.login()
+    account.create_other_resume()
+    account.check_visibility()
 
 
 @allure.epic('Authorized')
@@ -76,12 +64,9 @@ def test_resume_visibility_off():
 @allure.severity('normal')
 @allure.label('layer', 'ui')
 def test_auth():
-    with allure.step('Preconditions: open SJ site'):
-        base.open()
-    with allure.step('Login in SJ site'):
-        account.login()
-    with allure.step('Check successful login'):
-        account.check_login()
+    base.open()
+    account.login()
+    account.check_login()
 
 
 @allure.epic('Authorized')
@@ -91,10 +76,7 @@ def test_auth():
 @allure.severity('normal')
 @allure.label('layer', 'ui')
 def test_logout():
-    with allure.step('Preconditions: open SJ site'):
-        base.open()
-    with allure.step('Login and logout in SJ site'):
-        account.login()
-        account.logout()
-    with allure.step('Check logout'):
-        account.check_logout()
+    base.open()
+    account.login()
+    account.logout()
+    account.check_logout()
